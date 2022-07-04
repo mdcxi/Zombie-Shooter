19f508c1246c524243630fb15d64153add3d1ce0
Huong Chu <hannahkawaii1611@gmail.com>
2022-07-04 12:48:29 +0700

    First Commit

---
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyAI : MonoBehaviour
+{
+    
+    [SerializeField] float chaseRange = 5f;
+    [SerializeField] float turnSpeed = 5f;
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
+    EnemyHealth health;
+    Transform target;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        health = GetComponent<EnemyHealth>();
+        target = FindObjectOfType <PlayerHealth>().transform;
+    }
+
+    void Update()
+    {
+        if (health.IsDead())
+        {
+            enabled = false;
+            navMeshAgent.enabled = false;
+        }
+
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        if (isProvoked)
+        {
+            EngageTarget();
+        }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }  
+    }
+
+    public void OnDamageTaken ()
+    {
+        isProvoked = true;
+    }
+    private void EngageTarget()
+    {
+        FaceTarget();
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget()
+    {
+        GetComponent<Animator>().SetBool("attack", false);
+        //when we chase target, set the trigger "move" in our animator 
+        GetComponent<Animator>().SetTrigger("move");
+
+
+        //the enemy will chase the player
+        try
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
+        catch 
+        {
+            Debug.Log("Error!");
+        }      
+    }
+
+    private void AttackTarget()
+    {
+        GetComponent<Animator>().SetBool("attack", true);
+    }
+
+    private void FaceTarget ()
+    {
+        Vector3 direction =  (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+
+        //transform.rotation = where the target is, we need to rotate at a certain speed 
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+    }
+
+    void OnDrawGizmosSelected() 
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere (transform.position, chaseRange);
+    }
+}
